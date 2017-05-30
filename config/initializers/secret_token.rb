f00d3600dec4f36656043ef1378bf9d469b1e16e
VIIjordon@gmail.com <VIIJordon@gmail.com>
2017-05-30 08:43:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackBox::Application.config.secret_token = '8076b134b08bc1e727a6b3412bf2b1baf0d6f6ed84479fe561186759fbf3fe02af09eb189a8398f59b5c02dc9ca0fa8fc182d61fbebb73fbe2728b4b91735ec1'
